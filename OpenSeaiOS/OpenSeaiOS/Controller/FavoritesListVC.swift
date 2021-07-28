f94ff3237d6d4229b2d19d839786c8b4e4a4b54c
Mondale Felix <mondalefelix@gmail.com>
2021-07-27 23:48:26 -0700

    added tabbar with navigation controllers

---
+++ OpenSeaiOS/OpenSeaiOS/Controller/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  OpenSeaiOS
+//
+//  Created by Mondale on 7/27/21.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+
+
+
+}
---
+++ OpenSeaiOS/OpenSeaiOS/Controller/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  OpenSeaiOS
+//
+//  Created by Mondale on 7/27/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+    
+
+
+}
