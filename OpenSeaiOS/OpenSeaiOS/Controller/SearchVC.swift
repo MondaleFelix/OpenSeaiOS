857fdbca95f909f18b844d86cdb04134aa57d522
Mondale Felix <mondalefelix@gmail.com>
2021-07-28 21:21:59 -0700

    built out SearchVC with custom components

--- OpenSeaiOS/OpenSeaiOS/Controller/SearchVC.swift
+++ OpenSeaiOS/OpenSeaiOS/Controller/SearchVC.swift
@@ -9,12 +9,61 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    
+    let logoImageView = UIImageView()
+    let walletAddressTF = OSTextField()
+    let getNFTButton = OSButton(backgroundColor: .systemBlue, title: "Get NFTs")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+        
+        configureLogoImageView()
+        configureTextField()
+        configureNFTButton()
     }
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView(){
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        logoImageView.image = UIImage(named: "opensea")
 
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField(){
+        view.addSubview(walletAddressTF)
+        
+        NSLayoutConstraint.activate([
+            walletAddressTF.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            walletAddressTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            walletAddressTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            walletAddressTF.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    func configureNFTButton(){
+        view.addSubview(getNFTButton)
+        
+        NSLayoutConstraint.activate([
+            getNFTButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            getNFTButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            getNFTButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            getNFTButton.heightAnchor.constraint(equalToConstant: 50)
+        
+        ])
+    }
 
 }
---
+++ OpenSeaiOS/OpenSeaiOS/View/OSTextField.swift
@@ -0,0 +1,39 @@
+//
+//  OSTextField.swift
+//  OpenSeaiOS
+//
+//  Created by Mondale on 7/28/21.
+//
+
+import UIKit
+
+class OSTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter Wallet Address"
+    }
+}
